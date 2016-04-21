073c66d68019232c2ab314880455056ff8a25726
Zz <tears330@gmail.com>
2016-04-21 14:05:32 +0800

    update 34

--- task_2_34/js/control.js
+++ task_2_34/js/control.js
@@ -1,24 +1,38 @@
 var controller = {
 	// 对用户输入的命令进行解析并调用方块方法
 	execute : function(command) {
-		switch (command) {
-			case "GO":
+		var commandArr = command.split(" ");
+		switch (commandArr[0]) {
+			case "TUN":
+				controller.changeHead(commandArr[1]);
+				break;
+			case "MOV":
+				controller.changeHead(commandArr[1]);
 				player.playerGo();
 				break;
-			case "TUN LEF":
-				player.turnLeft();
-				break;
-			case "TUN RIG":
-				player.turnRight();
-				break;
-			case "TUN BAC":
-				player.turnBack();
-				break;
-			default : 
-				console.log("非法命令！");
+			case "GO":
+				player.playerGo();
+			default:
+				console.log("command false");
 				break;
 		}
 		render.refresh();
+	},
+	// 根据传入方向改变player.head方向
+	changeHead : function(directString) {
+		switch (directString) {
+			case "LEF":
+				player.turnLeft();
+				break;
+			case "RIG":
+				player.turnRight();
+				break;
+			case "BAC":
+				player.turnBack();
+				break;
+			default:
+				console.log("command false!")
+		}
 	},
 
 	// 为页面添加事件代理
